61540d738e8e8c88c37ca2fdbd724fc2d11a9989
Abegunde Precious Olanrewaju <preciousolanrewaju1998@gmail.com>
2022-07-04 17:08:57 +0100

    Update assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -14,7 +14,11 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // return arrayOfNumbers.length;
+    let sum = 0;
+    for(let arrayOfNumber of arrayOfNumbers){
+       return sum += arrayOfNumber;
+    }
 }
 assignment.sumOfNumbers = sumOfNumbers;
 
@@ -28,9 +32,22 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // return arrayOfNumbers.length;
+    let newArray = []
+    for(i=0;i<arrayOfNumbers.length;i++){
+        let checkEvenNumber = arrayOfNumbers[i] % 2;
+        if(checkEvenNumber === 0){
+            newArray[i] = arrayOfNumbers[i];
+        }else{
+            newArray[i] = " ";
+        }
+    }
+    newArray = newArray.filter(function(e) {
+        return String(e).trim();
+    });
+    return newArray.length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
